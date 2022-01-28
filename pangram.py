9c25fd56b143f4a13b552950c931a626c9a05878
aquellier <antoinecquellier@gmail.com>
2022-01-28 21:01:36 +0100

    pangram

---
+++ pangram.py
@@ -0,0 +1,23 @@
+import unittest
+import string
+
+
+def is_pangram(s):
+    lowercase = s.lower()
+    for letter in list(string.ascii_lowercase):
+        if letter not in lowercase:
+            return False
+    return True
+    # return set(string.lowercase) <= set(s.lower())
+
+
+class Test(unittest.TestCase):
+
+    def test_square_sum(self):
+        self.assertTrue(is_pangram('abcdefghijklmnopqrstuvwxyz'))
+        self.assertTrue(is_pangram('abcdefdsfdsffghijkLMNopqfdsfsrstuvwxyz'))
+        self.assertFalse(is_pangram('abcdefghijklmnopqrstuvwxy'))
+
+
+if __name__ == '__main__':
+    unittest.main()
